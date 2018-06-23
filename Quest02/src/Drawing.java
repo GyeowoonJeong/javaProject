4c23ce035769c86c4bfb6733ae16fcc3c7948463
GyeowoonJeong <21600631@handong.edu>
2018-06-23 23:21:03 +0900

    Inner ArrayList for pen trace

--- Quest02/src/Drawing.java
+++ Quest02/src/Drawing.java
@@ -17,17 +17,21 @@ import java.util.ArrayList;
 
 import javax.swing.JColorChooser;
 import javax.swing.JOptionPane;
+import javax.swing.ToolTipManager;
 
 public class Drawing extends Canvas implements MouseMotionListener, MouseListener, ActionListener{
 	protected Point first, second;
 	protected String tool = "Line";
 	protected Color c;
 	protected int stroke = 2;
+	protected int currentIdx;
 	private Color eraseC = Color.WHITE;
-	private ArrayList saver;
+	private ArrayList<Object> saver;
+	private ArrayList<PenTrace> trace;
 	
 	Drawing() {
 		this.saver = new ArrayList<>();
+		this.trace = new ArrayList<>();
 		this.setBackground(Color.WHITE);
 		this.addMouseListener(this); 
 		this.addMouseMotionListener(this);
@@ -65,18 +69,32 @@ public class Drawing extends Canvas implements MouseMotionListener, MouseListene
 	public void paint(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		super.paint(g2);
-		
+		int j;
 		for(int i = 0; i < saver.size(); i++) {
 			History drawhistory = (History)saver.get(i);
-			g2.setStroke(new BasicStroke(drawhistory.getStroke()));
-			g2.setColor(drawhistory.getColor());
-			g2.draw(drawhistory.getShape());
 			
+			if(drawhistory.getTool().equals("Pen")) {
+				for(j = 0; j < trace.size(); j++) {
+					if(trace.get(j).getObjIdx() == i)
+						break;
+				}
+				for(int k = j; k != i; k++) {
+					g2.setStroke(new BasicStroke(drawhistory.getStroke()));
+					g2.setColor(drawhistory.getColor());
+					g2.draw(new Line2D.Double(trace.get(k).getStart().x, trace.get(k).getStart().y, trace.get(k).getEnd().x, trace.get(k).getEnd().y));
+				}
+			}
+			
+			else {
+				g2.setStroke(new BasicStroke(drawhistory.getStroke()));
+				g2.setColor(drawhistory.getColor());
+				g2.draw(drawhistory.getShape());
+			}
 		}
 		
 		g2.setStroke(new BasicStroke(stroke));
 		if((first != null) && (second != null)) {
-			if(tool.equals("Line")) {
+			if(tool.equals("Pen") || tool.equals("Line")) {
 				g2.setColor(c);
 				g2.drawLine(first.x, first.y, second.x, second.y);
 			}
@@ -103,8 +121,14 @@ public class Drawing extends Canvas implements MouseMotionListener, MouseListene
 	
 	@Override
 	public void mouseDragged(MouseEvent e) {
+		Graphics2D g2 = (Graphics2D) getGraphics();
 		second = e.getPoint();
-		
+		if(tool.equals("Pen")) {
+			PenTrace pt = new PenTrace(currentIdx, first, second);
+			trace.add(pt);
+			g2.draw(new Line2D.Double(first.x, first.y, second.x, second.y));
+			first = second;
+		}
 		repaint();
 	}
 
@@ -112,6 +136,8 @@ public class Drawing extends Canvas implements MouseMotionListener, MouseListene
 	@Override
 	public void mousePressed(MouseEvent e) {
 		first = e.getPoint();
+		if(tool.equals("Pen"))
+			currentIdx = saver.size();
 	}
 
 	@Override
@@ -119,23 +145,31 @@ public class Drawing extends Canvas implements MouseMotionListener, MouseListene
 		Graphics2D g2 = (Graphics2D) getGraphics();
 		second = e.getPoint();
 		
-		if (tool.equals("Line")) {
+		if(tool.equals("Pen")) {
+			Line2D p = new Line2D.Double(first.x, first.y, second.x, second.y);
+			History pen = new History(tool, p, c, stroke);
+			saver.add(pen);
+		}
+		
+		else if (tool.equals("Line")) {
 			Line2D li = new Line2D.Double(first.x, first.y, second.x, second.y);
-			History line = new History(li, c, stroke);
+			History line = new History(tool, li, c, stroke);
 	        saver.add(line);
 	        g2.draw(li);
 		}
+		
 		else if(tool.equals("Rectangle")) {
 			Rectangle2D rect = new Rectangle2D.Double(Math.min(first.x, second.x), Math.min(first.y, second.y), Math.max(first.x, second.x) - Math.min(first.x, second.x), 
 					Math.max(first.y, second.y) - Math.min(first.y, second.y));
-			History rectangle = new History(rect, c, stroke);
+			History rectangle = new History(tool, rect, c, stroke);
 			saver.add(rectangle);
 			g2.draw(rect);
 		}
+		 
 		else if(tool.equals("Circle")) {
 			Ellipse2D el = new Ellipse2D.Double(Math.min(first.x, second.x), Math.min(first.y, second.y), Math.max(first.x, second.x) - Math.min(first.x, second.x), 
 					Math.max(first.y, second.y) - Math.min(first.y, second.y));
-			History ellipse = new History(el, c, stroke);
+			History ellipse = new History(tool, el, c, stroke);
 			saver.add(ellipse);
 			g2.draw(el);
 		}
